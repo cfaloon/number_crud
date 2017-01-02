9da3dd4b8ab73816e3ef548842d4db10fb3a92a4
cfaloon <cfaloon@gmail.com>
2017-01-02 12:28:24 -0500

    Write two unit tests for testing valid records.

--- test/models/number_record_test.rb
+++ test/models/number_record_test.rb
@@ -4,4 +4,18 @@ class NumberRecordTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "only intergers should be valid values" do
+    valid_number_record = NumberRecord.new(value: 1)
+    invalid_number_record = NumberRecord.new(value: 1.9)
+    assert valid_number_record.valid?
+    assert invalid_number_record.invalid?
+  end
+
+  test "only integers 1-10 should be valid" do
+    valid_number_record = NumberRecord.new(value: 9)
+    invalid_number_record = NumberRecord.new(value: 0)
+    assert valid_number_record.valid?
+    assert invalid_number_record.invalid?
+  end
 end
